54fc3cf1d18ba2d3dc9954b8d775f46f4d8439df
Remalloc <vbover@163.com>
2017-06-03 11:05:10 +0800

    Delete 1070. 结绳(25).py

--- PAT(乙级)/1070. 结绳(25).py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from functools import reduce
-input()
-node=input().strip().split(' ')
-node=list(map(int,node))
-node.sort()
-result=reduce(lambda x,y:x/2+y/2,node)
-print(int(result))
