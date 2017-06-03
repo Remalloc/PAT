d8edc824b5ec76d38ed375c6f44c87d9e5b3c1b8
vbover <vbover@163.com>
2017-06-03 11:49:29 +0800

    数字替换

---
+++ PAT(乙级)/1066. 图像过滤(15).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+y, x, min, max, repl = input().strip().split(" ")
+
+
+def judge_num(num, min=int(min), max=int(max), repl=int(repl)):
+    num=int(num)
+    if num >= min and num <= max:
+        return str(repl).zfill(3)
+    else:
+        return str(num).zfill(3)
+
+
+for i in range(int(y)):
+    cin=input().strip().split(' ')
+    re=[]
+    for j in cin:
+        re.append(judge_num(j))
+    print(" ".join(re))
+
